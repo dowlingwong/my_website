8ba962b4203ca09644102d7d40f1fc82c87b0780
Dowling_Wong <80257366+Dowling7@users.noreply.github.com>
2024-03-20 00:23:22 -0400

    latex

---
+++ academic_blog/script.js
@@ -0,0 +1,9 @@
+(function() {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.2.0/es5/tex-mml-chtml.min.js';
+    script.integrity = 'sha384-8q9JDTVUz9HK7J5ODv7mc9h2+nJE9Bq9yE6BJRgDFPZJ+Il8G0pDIDy9J+X2hb+q';
+    script.crossOrigin = 'anonymous';
+    script.async = true;
+    document.body.appendChild(script);
+})();
